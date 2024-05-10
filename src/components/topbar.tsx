b73cd0494bce4ee18312a18d7630dd8752ea17f1
IzaanAnwar <mdizaan67@gmail.com>
2024-05-10 16:28:56 +0530

    style: nav

---
+++ src/components/topbar.tsx
@@ -0,0 +1,10 @@
+import { Link } from "@nextui-org/link";
+export const TopBar = () => {
+    return <nav className="flex justify-between items-center px-2 md:px-56 lg:px-96 py-8 font-bold ">
+        <Link className="text-black text-lg">about me</Link>
+        <Link className="text-black text-lg">projects</Link>
+        <Link className="text-black text-lg">hire me</Link>
+        <Link className="text-black text-lg">socials</Link>
+    </nav>
+
+}
